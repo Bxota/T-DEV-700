72355f946f2adbce86b65757e68c6cdb648a7b7b
Bxota <thomas.pro.leterme@gmail.com>
2025-10-07 15:50:57 +0200

    add shifts/add

--- backend/api/users/shifts/views.py
+++ backend/api/users/shifts/views.py
@@ -2,9 +2,16 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from api.permissions import HasTeamTagPermission
+
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def list_shifts(request):
+    return Response([])
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated, HasTeamTagPermission])
+def add_shifts(request):
     return Response([])
 
 @api_view(["POST"])
