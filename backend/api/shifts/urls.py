a1b0bf9d6909578f7677f8114dc2b7f018c825e2
Bxota <thomas.pro.leterme@gmail.com>
2025-10-08 09:40:32 +0200

    add ApiView

--- backend/api/shifts/urls.py
+++ backend/api/shifts/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
 
-from api.shifts.views import list_user_shifts, shift_user_off, shift_user_on, add_user_shifts, list_team_shifts, add_team_shifts
+from api.shifts.views import TeamShiftCollection, UserShiftCollection, UserShiftDetail
 
 urlpatterns = [
-    path("users/<int:user_id>/shifts/", list_user_shifts, name="list_user_shifts"),
-    path("users/<int:user_id>/shifts/add", add_user_shifts, name="add_user_shifts"),
-    path("users/<int:user_id>/shifts/<int:shift_id>/on/", shift_user_on, name="shift_on"),
-    path("users/<int:user_id>/shifts/<int:shift_id>/off/", shift_user_off, name="shift_off"),
-    
-    path("teams/<int:team_id>/shifts/", list_team_shifts, name="list_team_shift"),
-    path("teams/<int:team_id>/shifts/add", add_team_shifts, name="add_team_shift"),
+    path("users/<int:user_id>/shifts/", UserShiftCollection.as_view(), name="user_shifts"),
+    path("users/<int:user_id>/shifts/<int:shift_id>", UserShiftDetail.as_view(), name="user_shift"),
+    path("teams/<int:team_id>/shifts/", TeamShiftCollection.as_view(), name="team_shifts")
 ]
-    
