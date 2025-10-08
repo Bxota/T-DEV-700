453a649f0d5a73208bd0280cc417a9f9fe22fc87
Bxota <thomas.pro.leterme@gmail.com>
2025-10-08 10:43:19 +0200

    commentaires sur les /reports

--- backend/api/users/urls.py
+++ backend/api/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from api.users.views import UserDetail, UserCollection
+from api.users.views import UserDetail, UserCollection, get_user_reports
 
 
 urlpatterns = [
     path("users/", UserCollection.as_view(), name="users"),
     path("users/<int:user_id>/", UserDetail.as_view(), name="user"),
+    path("users/<int:user_id>/reports", get_user_reports, name="user_reports"),
 ]
