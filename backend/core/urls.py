889c2a44091a231c271ee8977f1110505ecf8e06
Bxota <thomas.pro.leterme@gmail.com>
2025-10-08 10:52:24 +0200

    add /clocks endpoint

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -17,13 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 from drf_spectacular.views import (
-    SpectacularAPIView,
     SpectacularSwaggerView,
     SpectacularRedocView,
 )
