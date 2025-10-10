581049b39e2f64835eb6c8b35f76098d089bc18d
Bxota <thomas.pro.leterme@gmail.com>
2025-10-10 13:37:07 +0200

    Team Serializer

--- backend/api/teams/views.py
+++ backend/api/teams/views.py
@@ -12,6 +12,7 @@ from api.users.service import UserManager
 from api.shifts.service import ShiftManager
 from db_manager.serializers import TeamSerializer
 from db_manager.repositories.team_repository import TeamRepository
+from db_manager.serializers import TeamSerializer
 
 from drf_spectacular.utils import (
     extend_schema, extend_schema_view, OpenApiParameter
