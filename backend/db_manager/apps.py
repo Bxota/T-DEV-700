50ff1875a92d2d97da4aaa7a2733465f44d98cde
Emilien <emilien.dewilde@epitech.eu>
2025-10-16 11:22:45 +0200

    FEAT - add script to config roles on db

--- backend/db_manager/apps.py
+++ backend/db_manager/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DbManagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'db_manager'
+
+    def ready(self):
+        import db_manager.signals
